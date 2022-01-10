9cd57afed0f105ec7c5881e1abc190b07723101a
Верещагина Ирина Александровна <hjr462@mail.ru>
2022-01-11 00:42:17 +0700

    form

---
+++ University/Forms/Main_first_inpute_form.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace University.Forms
+{
+    partial class Main_first_inpute_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginBox = new System.Windows.Forms.TextBox();
+            this.PassBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LoginBox
+            // 
+            this.LoginBox.Location = new System.Drawing.Point(286, 134);
+            this.LoginBox.Multiline = true;
+            this.LoginBox.Name = "LoginBox";
+            this.LoginBox.Size = new System.Drawing.Size(234, 50);
+            this.LoginBox.TabIndex = 0;
+            // 
+            // PassBox
+            // 
+            this.PassBox.Location = new System.Drawing.Point(286, 211);
+            this.PassBox.Multiline = true;
+            this.PassBox.Name = "PassBox";
+            this.PassBox.PasswordChar = '*';
+            this.PassBox.Size = new System.Drawing.Size(234, 50);
+            this.PassBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(185, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ЛОГИН";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ПАРОЛЬ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(286, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 56);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ВОЙТИ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Main_first_inpute_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PassBox);
+            this.Controls.Add(this.LoginBox);
+            this.Name = "Main_first_inpute_form";
+            this.Text = "Main_first_inpute_form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LoginBox;
+        private System.Windows.Forms.TextBox PassBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
