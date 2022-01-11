2e0b58f4ad0291b8d23ad5b39a29210a51c946bd
Верещагина Ирина Александровна <hjr462@mail.ru>
2022-01-11 08:09:07 +0700

    old

--- University/Forms/Bank_asks.Designer.cs
+++ University/Forms/Bank_asks.Designer.cs
@@ -63,6 +63,7 @@ namespace University.Forms
             this.button1.TabIndex = 2;
             this.button1.Text = "Добавить";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Bank_asks
             // 
--- University/Forms/Bank_asks.cs
+++ University/Forms/Bank_asks.cs
@@ -19,9 +19,6 @@ namespace University.Forms
             if (flag)
             {
                 label1.Text = "Банк заданий";
-
-                // Tools.FillDG(dataGridView1, "SELECT * FROM task","");
-
                 Tools.FillDG(dataGridView1, "SELECT * FROM task a1 inner JOIN topic a2 on a1.task_pk = a2.topic_pk inner JOIN discipline a3 on a3.discipline_pk = a2.topic_pk inner JOIN entry_in_study_statement a4 on a3.discipline_pk = a4.entry_in_study_statement_pk inner JOIN study_statement_header a5 on a5.study_statement_header_pk = a4.entry_in_study_statement_pk WHERE a5.teacher_pk =" +  id+";", "");
             }
             else
@@ -43,22 +40,31 @@ namespace University.Forms
 
             if (dataGridView1.Columns[e.ColumnIndex].Name == "Delete")
             {
-                MessageBox.Show("Ты уверен?");
-               
+                
+                var a = ((DataTable)dataGridView1.DataSource).Rows;
+                var b = a[0][0];
 
+MessageBox.Show("Ты уверен? "+b);
             }
             else
                  if (dataGridView1.Columns[e.ColumnIndex].Name == "Change")
             {
                 MessageBox.Show("Дада, сейчас?");
                 var a = ((DataTable)dataGridView1.DataSource).Rows;
-                var b = a[0][1];
+                var b = a[0][0];
+                MessageBox.Show("Ты уверен? " + b);
                 //AddAndChange form = new AddAndChange(true, b.ToString());
-               // form.ShowDialog();
+                // form.ShowDialog();
 
             }
 
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        
+    }
     }
 }
